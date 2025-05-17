72e66f348ab3e7ada470440968a8f37f9d63dacd
Matheus Alves <matheusdesousaalves2018@gmail.com>
2025-05-17 15:53:44 -0300

    style: update TodoList layout for improved responsiveness

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -20,8 +20,8 @@ export function TodoList() {
   } = useTodos()
 
   return (
-    <div className="mt-4 space-y-4 w-full">
-      <Card>
+    <div className="mt-4 space-y-4 w-full flex flex-col items-center">
+      <Card className=" w-full max-w-xl">
         <CardHeader>
           <CardTitle>Tarefas</CardTitle>
           <TodoForm
